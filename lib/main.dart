c59aa478f5b5a790a029fd9e96113e3babe78c12
NotKidding <62922466+NotKidding@users.noreply.github.com>
2024-10-25 17:28:26 +0530

    Let's begin

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My App'),
+        ),
+        body: const Center(
+          child: Text('Hello, world!'),
+        ),
+      ),
+    );
   }
 }
