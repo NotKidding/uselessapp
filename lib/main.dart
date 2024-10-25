4968b07dee59d161c12abef2692578d86da184b3
NotKidding <62922466+NotKidding@users.noreply.github.com>
2024-10-25 21:48:58 +0530

    made sample notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,51 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  AwesomeNotifications().initialize(
+    null,//icon
+    [
+      NotificationChannel(
+        channelKey: 'TimeKey', 
+        channelName: 'Time', 
+        channelDescription: 'Notification Time',
+        ),
+    ],
+    debug: true,
+  );
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+@override
+void initState(){
+  AwesomeNotifications().isNotificationAllowed().then((isAllowed){
+    if(!isAllowed){
+      AwesomeNotifications().requestPermissionToSendNotifications();
+    }
+  });
+
+  super.initState();
+}
+
+void triggerNotification(){
+  AwesomeNotifications().createNotification(
+    content: NotificationContent(
+      id: 10,
+      channelKey: 'TimeKey',
+      title: 'Simple Notification',
+      body: 'Notification Body'
+
+    )
+  );
+}
 
   @override
   Widget build(BuildContext context) {
@@ -14,8 +54,10 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('My App'),
         ),
-        body: const Center(
-          child: Text('Hello, world!'),
+        body:  Center(
+          child: ElevatedButton(
+            onPressed: triggerNotification, 
+            child: const Text('Trigger Notification')),
         ),
       ),
     );
